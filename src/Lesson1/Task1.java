e61eae9dcf634e8fecb296d4d136f69b9c7a6fa1
kotoj <st.ostrovskis@gmail.com>
2023-05-09 14:22:50 +0300

    Lesson 1

---
+++ src/Lesson1/Task1.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("Значение a: " + a + ". Значение b: " + b + ".");
+    }
+
+}
---
+++ src/Lesson1/Task2.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String name = "Stas";
+        char S = 'S';
+        char t = 't';
+        char a = 'a';
+        char s = 's';
+
+        System.out.println(name);
+        System.out.print(S);
+        System.out.print(t);
+        System.out.print(a);
+        System.out.print(s);
+    }
+}
